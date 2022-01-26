43310236798fcf753de517b347cc5bf66e6f1f92
LionAli <sherali.abdukakhkharov@gmail.com>
2022-01-26 17:02:13 +0500

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: '.env.test' });
+} else {
+  dotenv.config();
+}
+module.exports = {
+  port: process.env.APP_PORT || 3000,
+  POSTGRES: {
+    host: process.env.POSTGRES_DB_HOST,
+    port: process.env.POSTGRES_DB_PORT,
+    user: process.env.POSTGRES_DB_USER,
+    password: process.env.POSTGRES_DB_PASSWORD,
+    database: process.env.POSTGRES_DB_NAME,
+  },
+  JWT: {
+    key: process.env.JWT_SECRET_KEY,
+    expiration: process.env.JWT_EXPIRATION,
+  },
+};
